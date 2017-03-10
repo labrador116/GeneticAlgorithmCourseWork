8cf29c84a8e502ca90eaed4c15ab39aa1889fd9a
Markin Andrey <magazon4ik@yandex.ru>
2017-03-10 09:03:37 +0300

    Добавьте файлы проекта.

---
+++ GeneticAlgorithmCourseWork/Adapter/ChromosomesAdapter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GeneticAlgorithmCourseWork.ChromosomeModel;
+
+namespace GeneticAlgorithmCourseWork.Adapter
+{
+    class ChromosomesAdapter
+    {
+        private List<ChromosomeModel.Chromosome> _container;
+   
+        public ChromosomesAdapter()
+        {
+            _container = new List<Chromosome>();
+        }
+
+        public List<Chromosome> Container { get => _container; set => _container = value; }
+    }
+}
---
+++ GeneticAlgorithmCourseWork/ChromosomeModel/Chromosome.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneticAlgorithmCourseWork.ChromosomeModel
+{
+   public class Chromosome
+    {
+        int _radius;
+        int _oX;
+        int _oY;
+
+        public Chromosome(int radius, int oX, int oY)
+        {
+            Radius = radius;
+            OX = oX;
+            this.OY = oY;
+        }
+        public int Radius { get => _radius; set => _radius = value; }
+        public int OX { get => _oX; set => _oX = value; }
+        public int OY { get => _oY; set => _oY = value; }
+   
+    }
+}
